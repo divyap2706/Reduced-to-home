ce24ca032d78fb98e2c7975c4a7ff12446b0e325
Alex Biehl <32490273+ab7289@users.noreply.github.com>
2021-04-27 22:23:13 -0400

    fixed spark job so that all output csv's don't have the same parameter, updated the EPA visualization notebook for new project structure and changed filenames

--- src/cleaning_src/Spark_Jobs/epa_air_split.py
+++ src/cleaning_src/Spark_Jobs/epa_air_split.py
@@ -32,64 +32,36 @@ def main():
         "pm25a_2019_avg_df": "parameter like 'Acceptable PM2.5%' AND year(date_local) = 2019 AND sample_duration like '24-HR%'",
         "ozone_2019_avg_df": "parameter like 'Ozone' AND year(date_local) = 2019 AND sample_duration like '%8-HR%'",
         "pm25_local_2019_avg_df": "parameter like 'PM2.5 - Local%' AND year(date_local) = 2019 AND sample_duration like '24 HOUR'",
-        "all_parm_2019_df": "year(date_local) = 2019 AND aqi IS NOT NULL",
+        # "all_parm_2019_df": "year(date_local) = 2019 AND aqi IS NOT NULL",
     }
     outputs_2020 = {
         "pm25a_2020_avg_df": "parameter like 'Acceptable PM2.5%' AND year(date_local) = 2020 AND sample_duration like '24-HR%'",
         "ozone_2020_avg_df": "parameter like 'Ozone' AND year(date_local) = 2020 AND sample_duration like '%8-HR%'",
         "pm25_local_2020_avg_df": "parameter like 'PM2.5 - Local%' AND year(date_local) = 2020 AND sample_duration like '24 HOUR'",
         "co_2020_avg_df": "parameter like 'Carbon monoxide' AND sample_duration like '8-HR%'",
-        "all_parm_2020_df": "year(date_local) = 2020 and aqi IS NOT NULL",
+        # "all_parm_2020_df": "year(date_local) = 2020 and aqi IS NOT NULL",
     }
 
+    frames_2019 = []
+    frames_2020 = []
     for k, v in outputs_2019.items():
-        aggregate(air_2019_df, v, k)
+        frames_2019.append(aggregate(air_2019_df, v, k))
 
     for k, v in outputs_2020.items():
-        aggregate(air_2020_df, v, k)
+        frames_2020.append(aggregate(air_2020_df, v, k))
 
-    # get PM2.5 values for 2019
-    # pm25a_2019_avg_df = air_2019_df.filter(
-    #     "parameter like 'Acceptable PM2.5%' AND year(date_local) = 2019 AND sample_duration like '24-HR%'"
-    # ).select("parameter", "date_local", "units_of_measure", "arithmetic_mean", "aqi")
-    # get PM2.5 values for 2020
-    # pm25a_2020_avg_df = air_2020_df.filter(
-    #     "parameter like 'Acceptable PM2.5%' AND year(date_local) = 2020 AND sample_duration like '24-HR%'"
-    # ).select("parameter", "date_local", "units_of_measure", "arithmetic_mean", "aqi")
+    # union all the filtered datasets so we can view all the parameters
+    # in one graph
+    all_parms_2019_df = frames_2019[0]
+    for i in range(1, len(frames_2019)):
+        all_parms_2019_df = all_parms_2019_df.union(frames_2019[i])
 
-    # get ozone values for 2019
-    # ozone_2019_avg_df = air_2019_df.filter(
-    #     "parameter like 'Ozone' AND year(date_local) = 2019 AND sample_duration like '%8-HR%'"
-    # ).select("parameter", "date_local", "units_of_measure", "arithmetic_mean", "aqi")
-    # get ozone values for 2020
-    # ozone_2020_avg_df = air_2020_df.filter(
-    #     "parameter like 'Ozone' AND year(date_local) = 2020 AND sample_duration like '%8-HR%'"
-    # ).select("parameter", "date_local", "units_of_measure", "arithmetic_mean", "aqi")
+    all_parms_2020_df = frames_2020[0]
+    for i in range(1, len(frames_2020)):
+        all_parms_2020_df = all_parms_2020_df.union(frames_2020[i])
 
-    # get Local PM2.5 values for 2019
-    # pm25_local_2019_avg_df = air_2019_df.filter(
-    #     "parameter like 'PM2.5 - Local%' AND year(date_local) = 2019 AND sample_duration like '24 HOUR'"
-    # ).select("parameter", "date_local", "units_of_measure", "arithmetic_mean", "aqi")
-    # get Local PM2.5 values for 2020
-    # pm25_local_2020_avg_df = air_2020_df.filter(
-    #     "parameter like 'PM2.5 - Local%' AND year(date_local) = 2020 AND sample_duration like '24 HOUR'"
-    # ).select("parameter", "date_local", "units_of_measure", "arithmetic_mean", "aqi")
-
-    # get Carbon Monoxide values for 2020, since there is no 2019 data
-    # co_2020_avg_df = air_2020_df.filter(
-    #     "parameter like 'Carbon monoxide' AND sample_duration like '8-HR%'"
-    # ).select("parameter", "date_local", "units_of_measure", "arithmetic_mean", "aqi")
-
-    # get data for all parameters for 2019 and 2020
-    # all_parm_2019_df = air_2019_df.filter(
-    #     "year(date_local) = 2019 AND aqi IS NOT NULL"
-    # ).select("parameter", "date_local", "units_of_measure", "arithmetic_mean", "aqi")
-
-    # all_parm_2020_df = air_2020_df.filter(
-    #     "year(date_local) = 2020 and aqi IS NOT NULL"
-    # ).select("parameter", "date_local", "units_of_measure", "arithmetic_mean", "aqi")
-
-    # write output so we can graph it
+    to_csv(all_parms_2019_df, "all_parms_2019_df")
+    to_csv(all_parms_2020_df, "all_parms_2020_df")
 
     spark.stop()
 
@@ -99,19 +71,22 @@ def aggregate(df, filter_query, output_name):
     creates a new column called 'month', aggregates the aqi and arithmetic_mean
     by month, then outputs to csv
     """
-    filtered_df = (
-        df.filter(filter_query)
-        .select("parameter", "date_local", "units_of_measure", "arithmetic_mean", "aqi")
-        .withColumn("month", date_format(col("date_local"), "M/yyyy"))
+    filtered_df = df.filter(filter_query).select(
+        "parameter", "date_local", "units_of_measure", "arithmetic_mean", "aqi"
+    )
+    units = filtered_df.select("units_of_measure").first()["units_of_measure"]
+    param = filtered_df.select("parameter").first()["parameter"]
+    agg_df = (
+        filtered_df.withColumn("month", date_format(col("date_local"), "M/yyyy"))
         .groupBy("month")
         .agg({"arithmetic_mean": "avg", "aqi": "avg"})
-        .withColumn(
-            "units_of_measure",
-            lit(df.select("units_of_measure").first()["units_of_measure"]),
-        )
-        .withColumn("parameter", lit(df.select("parameter").first()["parameter"]))
+        .withColumnRenamed("avg(aqi)", "aqi")
+        .withColumnRenamed("avg(arithmetic_mean)", "arithmetic_mean")
+        .withColumn("units_of_measure", lit(units))
+        .withColumn("parameter", lit(param))
     )
-    to_csv(filtered_df, output_name)
+    to_csv(agg_df, output_name)
+    return agg_df
 
 
 def to_csv(df, output_name):
